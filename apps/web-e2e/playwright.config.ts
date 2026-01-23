a5f71df9e2ca06dcbdde16f306bf3c71950f1974
Sergi Luque <sergi1008@gmail.com>
2026-01-23 20:30:28 +0100

    Add end-to-end testing script to package.json and comment out webkit configuration in playwright.config.ts

--- apps/web-e2e/playwright.config.ts
+++ apps/web-e2e/playwright.config.ts
@@ -40,10 +40,12 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
+    /*
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    */
 
     // Uncomment for mobile browsers support
     /* {
